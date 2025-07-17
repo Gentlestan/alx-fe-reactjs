fd50bf5d2f0c1fd998f842a37904a3a56b3ce5b5
Ohazulike Stanley <sohazulike@gmail.com>
2025-07-17 22:05:22 +1000

    add alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function MainContent() {
+  return (
+    <main style={{border: '1px solid brown', backgroundColor: 'black', color: 'white', padding: '10px', margin: '10px'}}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+export default MainContent
