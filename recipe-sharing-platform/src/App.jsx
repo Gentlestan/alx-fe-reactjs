957bec2fe1f7da2751409dc5c3fc3e24470e6909
Ohazulike Stanley <sohazulike@gmail.com>
2025-08-14 04:58:44 +1000

    add recipedetails.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Home } from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail'
 import './index.css'
 import './App.css'
 
 function App() {
- 
-
   return (
-    <div><Home/></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   )
 }
 
 export default App
+
+ 
+
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,60 +1,22 @@
-import React, {useState, useEffect} from 'react'
-import bitterleaf  from "../assets/images/bitter-leaf.jpeg"
-import boli from "../assets/images/boli-groundnut.jpeg"
-import efoRiro from "../assets/images/efo-riro.jpeg"
-import egusiSoup from "../assets/images/egusi-soup.jpeg"
-import jollofRice from "../assets/images/jollof-rice.jpeg"
-import moi from "../assets/images/moi-moi.jpeg"
-import okra from "../assets/images/okro-soup.jpeg"
-import suya from "../assets/images/suya.jpeg"
-import nsala from "../assets/images/nsala-soup.jpeg"
-import nkwobi from "../assets/images/nkwobi.jpeg"
-
-import recipesData from "../data.json"
-
-
+import { recipesWithImages } from "../utils/recipeWithImage";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  const [recipes, setRecipes] = useState([])
-
-  const imageMap = {
-    "https://via.placeholder.com/150": [
-      jollofRice,
-      egusiSoup,
-      suya,
-      efoRiro,
-      moi,
-      bitterleaf,
-      boli,
-      okra,
-      nsala,
-      nkwobi,
-    ],
-  };
-
-  useEffect(() => {
-    // Replace image URLs with local imports
-    const updatedRecipes = recipesData.map((recipe, index) => ({
-      ...recipe,
-      image: imageMap["https://via.placeholder.com/150"][index],
-    }));
-    setRecipes(updatedRecipes);
-  }, []);
-
   return (
-    <div className='text-white grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 p-2'>
-        {recipes.map((recipe)=>
-            <div className='max-w-sm mx-auto bg-gray-800 p-4 rounded-lg shadow'>
-              <h2 className='text-center mb-2'>{recipe.title}</h2>
+    <div className="text-white grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 p-2">
+      {recipesWithImages.map((recipe) => (
+        <Link to={`/recipe/${recipe.id}`} className="block h-full" key={recipe.id}>
+          <div className="max-w-sm mx-auto bg-gray-800 p-4 rounded-lg shadow flex flex-col h-full">
+            <h2 className="text-center mb-2">{recipe.title}</h2>
             <img
-            src={recipe.image}
-            alt={recipe.title}
-            className="mb-2 w-36 h-36 object-cover rounded-full mx-auto bg-white overflow-hidden shadow-md hover:shadow-xl hover:scale-105 transition-transform duration-300"
+              src={recipe.image}
+              alt={recipe.title}
+              className="mb-2 w-36 h-36 object-cover rounded-full mx-auto bg-white overflow-hidden shadow-md hover:shadow-xl hover:scale-105 transition-transform duration-300"
             />
-              <p className='text-sm'>{recipe.summary}</p>
-              
-            </div>
-        )}
+            <p className="text-sm">{recipe.summary}</p>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
