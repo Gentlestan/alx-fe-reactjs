182f9fc65220979b94e4b8a8ab032cb4f66562a8
Ohazulike Stanley <sohazulike@gmail.com>
2025-08-25 08:50:30 +1000

    add test.js

---
+++ react-todo/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+};
